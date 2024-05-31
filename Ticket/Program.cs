16ebdb42f830962bdc6afe96acd8edbac5a70cb7
Frank Appiah <foappiah009@st.ug.edu.gh>
2024-05-31 20:57:08 +0000

    Ticket

---
+++ Ticket/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            decimal ticketPrice;
+            if (age >= 65 || age <= 12)
+                ticketPrice = 7.00m;
+            else
+                ticketPrice = 10.00m;
+
+            Console.WriteLine($"Your ticket price is: GHC{ticketPrice}");
+        }
+    }
+}
